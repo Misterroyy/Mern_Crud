157edf3350121a732d150fbfe8b5c09eac83f5de
Misterroyy <110715313+Misterroyy@users.noreply.github.com>
2023-08-04 13:59:00 +0530

    final code
    no description

---
+++ server/index.js
@@ -0,0 +1,77 @@
+const express = require('express')
+const cors = require('cors')
+const mongoose = require('mongoose')
+const path = require('path')
+
+const app = express()
+app.use(cors())
+app.use (express. json())
+
+//static files 
+app.use (express.static(path.join(__dirname, './client/build')));
+
+app.get('*' , function(req , res){
+    res.sendFilepath.join(__dirname, "./client/build/index.html");
+});
+
+const PORT = process.env.PORT || 8080
+//  schema
+const schemaData = mongoose.Schema({
+    name:String,
+    title : String,
+    description : String,
+    status : String,
+    
+},{
+    timestamps : true
+})
+
+const userModel = mongoose.model("crud" , schemaData)
+
+//read
+app.get("/" , async(req , res)=>{
+   const data = await userModel.find({})
+   res.json ({success : true , data : data})
+})
+
+// create data 
+app.post("/create", async(req , res)=>{
+    console.log(req.body)
+    const data = new userModel(req.body)
+    await data.save()
+    res.send({
+        success:true ,message :"data save successfully" , data: data
+    })
+})
+
+// update
+app.put("/update" , async(req ,res)=>{
+   console.log(req.body)
+   const { _id,...rest} = req.body
+   console.log(rest)
+    const data = await userModel .updateOne({_id : _id}, rest)
+   res.send({success : true , message : "data updated successfully" , data : data})
+})
+
+// delete
+
+app.delete("/delete/:id" , async(req,res)=>{
+    const id = req.params.id
+    console.log(id)
+    const data = await userModel.deleteOne({_id : id})
+   res.send({success : true , message : "data deleted  successfully" ,data : data})
+
+})
+
+
+
+
+mongoose.connect("mongodb://127.0.0.1:27017/crud")
+.then(()=>{
+    console.log("connected to db ") 
+    app.listen(PORT,()=>console.log("server is running"))
+
+
+})
+
+.catch((err)=>console.log(err))
